db7bce542b13628414f7f64d13cf81604d10b386
zeoio <ifordo@outlook.com>
2024-06-27 17:25:21 +0800

    script/ add taiko network

--- script/deployParameters/DeployTaiko.s.sol
+++ script/deployParameters/DeployTaiko.s.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.15;
 import {DeployUniversalRouter} from '../DeployUniversalRouter.s.sol';
 import {RouterParameters} from 'contracts/base/RouterImmutables.sol';
 
-contract DeployBaseSepolia is DeployUniversalRouter {
+contract DeployTaiko is DeployUniversalRouter {
     function setUp() public override {
         params = RouterParameters({
             permit2: 0xB952578f3520EE8Ea45b7914994dcf4702cEe578,
-            weth9: 0x6267947C818ff3900F620FC97d590702afB69147,
+            weth9: 0xA51894664A773981C6C112C43ce576f315d5b1B6,
             seaportV1_5: UNSUPPORTED_PROTOCOL,
             seaportV1_4: UNSUPPORTED_PROTOCOL,
             openseaConduit: UNSUPPORTED_PROTOCOL,
@@ -23,12 +23,12 @@ contract DeployBaseSepolia is DeployUniversalRouter {
             routerRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareRewardsDistributor: UNSUPPORTED_PROTOCOL,
             looksRareToken: UNSUPPORTED_PROTOCOL,
-            v2Factory: 0x61b781d983f5D2134946dF130E9B62962C71b2C0,
-            v3Factory: 0xfaa89f95CE1948582154ee1C0Ea47701cBb95E04,
+            v2Factory: 0x2EA9051d5a48eA2350b26306f2b959D262cf67e1,
+            v3Factory: 0xfCA1AEf282A99390B62Ca8416a68F5747716260c,
             pairInitCodeHash: 0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46,
             poolInitCodeHash: 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54
         });
 
-        unsupported = 0x8799399495c9d25dcb4a4e2A666fd66548Ab318d;
+        unsupported = 0x3F488103f100D95c496F5aD83Ad363a547968097;
     }
 }
